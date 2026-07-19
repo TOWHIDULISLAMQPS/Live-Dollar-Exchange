b27dba630cac65832e5d46aac258c91f7ca9e1f8
TOWHIDULISLAMQPS <towhidulislamtuhin872@gmail.com>
2026-07-20 00:35:26 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,18 @@
+// Firebase SDK
+
 import { initializeApp } from 
-"https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+"https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 
 import { getDatabase } from 
-"https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
+"https://www.gstatic.com/firebasejs/12.16.0/firebase-database.js";
+
+import { getAuth } from 
+"https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
 
 
 const firebaseConfig = {
 
-apiKey: "আপনার apiKey",
+apiKey: "AIzaSyBC0OA7gEWg1dfVzqVpI1YgbKzNKllz4pg",
 
 authDomain: "ts-dollar-exchange.firebaseapp.com",
 
@@ -19,7 +24,9 @@ storageBucket: "ts-dollar-exchange.firebasestorage.app",
 
 messagingSenderId: "458864027860",
 
-appId: "1:458864027860:web:b5a0e671b4ca8f53bfb341"
+appId: "1:458864027860:web:adfe8163cbe6bddfbfb341",
+
+measurementId: "G-1SHL91C6Z0"
 
 };
 
@@ -28,5 +35,7 @@ const app = initializeApp(firebaseConfig);
 
 const db = getDatabase(app);
 
+const auth = getAuth(app);
 
-export { app, db };
+
+export { app, db, auth };
