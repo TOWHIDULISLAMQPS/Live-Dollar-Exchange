25c2dadebeeaa1088ee1fc44b058302687a4061a
TOWHIDULISLAMQPS <towhidulislamtuhin872@gmail.com>
2026-07-19 13:34:50 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function calculate(){
 
+let usd=document.getElementById("usd").value;
+
+let rate=122;
+
+let total=usd*rate;
+
+document.getElementById("result").innerHTML=
+usd+" USD = "+total.toFixed(2)+" BDT";
+
+}
