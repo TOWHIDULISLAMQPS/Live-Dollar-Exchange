137ce548b8ed0674145c10561b5f719af4cfb468
TOWHIDULISLAMQPS <towhidulislamtuhin872@gmail.com>
2026-07-22 23:07:35 +0600

    Update admin-orders.js

--- admin-orders.js
+++ admin-orders.js
@@ -1,8 +1,7 @@
-import { db } from "./firebase.js";
-
 import {
     ref,
-    onValue
+    onValue,
+    update
 } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-database.js";
 
 // ==========================
